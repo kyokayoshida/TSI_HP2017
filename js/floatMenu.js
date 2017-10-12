4e091563cfdb8724fdf2d24e47d8da6fe65a4b6c
kyokayoshida <k-yoshida@tsis.co.jp>
2017-10-12 10:54:08 +0900

    add

---
+++ js/floatMenu.js
@@ -0,0 +1,77 @@
+
+$(function() {
+	 $('#nav2 li a').hover(
+		function(){
+			$(this).find('span').stop().animate({'marginRight':'175px'},500);
+		},
+		function () {
+			$(this).find('span').stop().animate({'marginRight':'0px'},300);
+		}
+	);
+});
+
+$(function() {
+	 $('#nav2_w li a').hover(
+		function(){
+			$(this).find('span').stop().animate({'marginRight':'175px'},500);
+		},
+		function () {
+			$(this).find('span').stop().animate({'marginRight':'0px'},300);
+		}
+	);
+});
+
+$(function() {
+	 $('#nav2_b li a').hover(
+		function(){
+			$(this).find('span').stop().animate({'marginRight':'175px'},500);
+		},
+		function () {
+			$(this).find('span').stop().animate({'marginRight':'0px'},300);
+		}
+	);
+});
+
+$(function() {
+	 $('#nav2_q li a').hover(
+		function(){
+			$(this).find('span').stop().animate({'marginRight':'175px'},500);
+		},
+		function () {
+			$(this).find('span').stop().animate({'marginRight':'0px'},300);
+		}
+	);
+});
+
+$(function() {
+	 $('#nav2_c_n li a').hover(
+		function(){
+			$(this).find('span').stop().animate({'marginRight':'175px'},500);
+		},
+		function () {
+			$(this).find('span').stop().animate({'marginRight':'0px'},300);
+		}
+	);
+});
+
+$(function() {
+	 $('#nav2_n_n li a').hover(
+		function(){
+			$(this).find('span').stop().animate({'marginRight':'175px'},500);
+		},
+		function () {
+			$(this).find('span').stop().animate({'marginRight':'0px'},300);
+		}
+	);
+});
+
+$(function() {
+	 $('#nav2_f li a').hover(
+		function(){
+			$(this).find('span').stop().animate({'marginRight':'175px'},500);
+		},
+		function () {
+			$(this).find('span').stop().animate({'marginRight':'0px'},300);
+		}
+	);
+});
